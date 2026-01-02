86f149b21a518bcacf7d205472273f396294c8f4
Christian Mamplata <201923244+Serial-Cyn@users.noreply.github.com>
2026-01-03 07:44:49 +0800

    chore(navbar): remove unused button and adjust padding

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,6 @@
-
 export default function Navbar() {
     return (
-        <nav className="sticky flex px-6 py-4 min-w-full bg-zinc-900 rounded-b-md justify-between items-center">
+        <nav className="sticky flex p-6 min-w-full bg-zinc-900 rounded-b-md justify-between items-center">
             {/* COMPANY LOGO OR BRANDING */}
             <div id="logo">
                 TailOps
@@ -12,12 +11,6 @@ export default function Navbar() {
                     <li className="hover:text-neutral-400 cursor-pointer">Documentation</li>
                     <li className="hover:text-neutral-400 cursor-pointer">About Us</li>
                     <li className="hover:text-neutral-400 cursor-pointer">Contact</li>
-                    {/* CTA */}
-                    <li>
-                        <button className="bg-foreground hover:bg-neutral-400 px-6 py-2 text-zinc-950 rounded-full cursor-pointer hover:scale-105 transition-transform duration-200">
-                            Log in
-                        </button>
-                    </li>
                 </ul>
             </div>
         </nav>
